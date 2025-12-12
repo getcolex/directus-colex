b71a625c3937991b265575b1fcf4e01979a2301a
AceShiken <gargaryayon2@gmail.com>
2025-12-12 14:15:22 +0530

    resolve dependency errors

--- packages/extensions-sdk/src/cli/commands/build.ts
+++ packages/extensions-sdk/src/cli/commands/build.ts
@@ -9,17 +9,17 @@ import {
 import type { AppExtensionType, ApiExtensionType } from '@directus/types';
 import { APP_EXTENSION_TYPES, EXTENSION_TYPES, HYBRID_EXTENSION_TYPES } from '@directus/constants';
 import { isIn, isTypeIn } from '@directus/utils';
-import commonjsDefault from '@rollup/plugin-commonjs';
-import jsonDefault from '@rollup/plugin-json';
+import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
-import replaceDefault from '@rollup/plugin-replace';
-import terserDefault from '@rollup/plugin-terser';
-import virtualDefault from '@rollup/plugin-virtual';
+import replace from '@rollup/plugin-replace';
+import terser from '@rollup/plugin-terser';
+import virtual from '@rollup/plugin-virtual';
 import vue from '@vitejs/plugin-vue';
 import chalk from 'chalk';
-import fse from 'fs-extra';
+import * as fse from 'fs-extra';
 import ora from 'ora';
-import path from 'path';
+import * as path from 'path';
 import type { RollupError, RollupOptions, OutputOptions as RollupOutputOptions } from 'rollup';
 import { rollup, watch as rollupWatch } from 'rollup';
 import esbuild from 'rollup-plugin-esbuild';
@@ -32,12 +32,7 @@ import generateBundleEntrypoint from './helpers/generate-bundle-entrypoint.js';
 import loadConfig from './helpers/load-config.js';
 import { validateSplitEntrypointOption } from './helpers/validate-cli-options.js';
 
-// Workaround for https://github.com/rollup/plugins/issues/1329
-const virtual = virtualDefault as unknown as typeof virtualDefault.default;
-const commonjs = commonjsDefault as unknown as typeof commonjsDefault.default;
-const json = jsonDefault as unknown as typeof jsonDefault.default;
-const replace = replaceDefault as unknown as typeof replaceDefault.default;
-const terser = terserDefault as unknown as typeof terserDefault.default;
+// Direct imports from Rollup plugins
 
 type BuildOptions = {
 	type?: string;
